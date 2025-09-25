bd9a3de9469d480c1161207cce3d59b9cc2d86c7
ns5504 <nawalsarwar5504@gmail.com>
2025-09-24 20:26:18 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'node:path'
-
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
-  }
+base: process.env.VITE_BASE_PATH || '/',
+plugins: [react()],
+resolve: { alias: { '@': path.resolve(__dirname, 'src') } }
 })
-
